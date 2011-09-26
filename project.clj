4efeb0f34af1c205723c9bde545ce8282a876ce0
Ivan Toshkov <ivan@toshkov.org>
2011-09-26 13:49:50 +0300

    Updated the project description

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject games "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Translation of java 2d games tutorial in clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]])
